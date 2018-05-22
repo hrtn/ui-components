de5657917e5fae75e68cd11b2ad8450905f784c7
hrtn <masaki-haruta@c-fo.com>
2018-05-22 11:02:53 +0900

    No1739

--- src/components/atoms/Heading/index.js
+++ src/components/atoms/Heading/index.js
@@ -1,31 +1,62 @@
-import React from 'react';
-import styles from './styles.css';
+import React from "react";
+import styles from "./styles.css";
 
-export const HeadingPresenter =({
-  tag:Tag,
+export const HeadingPresenter = ({
+  tag: Tag,
   visualLevel,
   className,
-  ...props,
+  ...props
 }) => (
-  <Tag className={[ styles.h, styles[`h${ visualLevel }`], className ].join(' ')}
-  { ...props } />
+  <Tag
+    className={[styles.h, styles[`h${visualLevel}`], className].join(" ")}
+    {...props}
+  />
+);
+
+export const HeadingUnderlinedPresenter = ({
+  tag: Tag,
+  visualLevel,
+  className,
+  ...props
+}) => (
+  <Tag
+    className={[
+      styles.h,
+      styles.underlined,
+      styles[`h${visualLevel}`],
+      className
+    ].join(" ")}
+    {...props}
+  />
 );
 
 export const HeadingContainer = ({
   presenter,
   level = 2,
   visualLevel,
-  ...props,
+  ...props
 }) => {
   level = Math.max(1, Math.min(6, level));
-  visualLevel = (typeof visualLevel !== 'undefined') ? visualLevel : level;
-  const tag = `h${ level }`;
+  visualLevel = typeof visualLevel !== "undefined" ? visualLevel : level;
+  const tag = `h${level}`;
 
   return presenter({ tag, visualLevel, ...props });
 };
 
+export const HeadingUnderlined = props => (
+  <HeadingContainer
+    presenter={presenterProps => (
+      <HeadingUnderlinedPresenter {...presenterProps} />
+    )}
+    {...props}
+  />
+);
+
 const Heading = props => (
-  <HeadingContainer presenter={ presenterProps => <HeadingPresenter { ...presenterProps } /> } { ...props } />
+  <HeadingContainer
+    presenter={presenterProps => <HeadingPresenter {...presenterProps} />}
+    {...props}
+  />
 );
 
 export default Heading;
